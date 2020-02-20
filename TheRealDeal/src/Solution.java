3a1205ccbb071fb50403f488640afd871fbf8923
porzack <zack@zackporter.com>
2020-02-20 11:08:35 -0800

    changes

--- TheRealDeal/src/Solution.java
+++ TheRealDeal/src/Solution.java
@@ -1,6 +1,15 @@
+import java.io.IOException;
+
 public class Solution {
 	
-	public static void main(String[] args) {
-		System.out.println("Hello google!");
+	public static void main(String[] args) throws IOException {
+		System.out.println("Hello Google!");
+		InputParser parser = new InputParser();
+        String filename = "/home/zack/Downloads/b_read_on.txt";
+        parser.parseInput(filename);
+        System.out.println(parser.numBooks);
+        System.out.println(parser.numLibraries);
+        System.out.println(parser.numDays);
+		System.out.println("Ended");
 	}
 }
