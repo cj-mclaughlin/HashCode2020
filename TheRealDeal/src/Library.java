21b4941cc571e8507dce304ae56d30f93d8e16d8
Nour Elmaliki <nourelmaliki@gmail.com>
2020-02-20 13:52:55 -0500

    Library Value getter

--- TheRealDeal/src/Library.java
+++ TheRealDeal/src/Library.java
@@ -1,23 +1,42 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Library {
     public int id;
-    public ArrayList<Book> books;
+    public ArrayList<Integer> books;
     public int booksPerDay;
     public int signupTime;
     public boolean signedUp;
 
     public Library(int id, int booksPerDay, int signupTime) {
-        this.books = new ArrayList<Book>();
+        this.books = new ArrayList<Integer>();
         this.id = id;
         this.booksPerDay = booksPerDay;
         this.signupTime = signupTime;
         this.signedUp = false;
     }
 
-    public void addBook(Book b) {
+    public void addBook(Integer b) {
         books.add(b);
     }
 
+
+    public int extractValue(Set<Integer> toBeScanned,HashMap<Integer,Integer> globalBooks, int days){
+        int daysRemaining = days - signupTime;
+        int maxValue = 0;
+        int currentBook = 0;
+        //implement sorting rn
+        while(daysRemaining!=0 && currentBook<books.size()){
+            if(!toBeScanned.contains(books.get(currentBook))){
+                maxValue += globalBooks.get(books.get(currentBook));
+                daysRemaining--;
+                currentBook++;
+            }
+            currentBook++;
+        }
+        return maxValue;
+    }
 }
