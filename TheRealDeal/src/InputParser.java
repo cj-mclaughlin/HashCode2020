0bf2fdb5cb7529260f1d7dd869feaa146ffc0e9c
Connor McLaughlin <cjmclaughlin@wpi.edu>
2020-02-20 13:55:41 -0500

    initial inputparser complete

--- TheRealDeal/src/InputParser.java
+++ TheRealDeal/src/InputParser.java
@@ -1,11 +1,56 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class InputParser {
-    // TODO add more attributes from problem description
-    ArrayList<Library> libraries;
+    public int numBooks;
+    public int numLibraries;
+    public int numDays;
+    public HashMap<Integer, Book> books;
+    public ArrayList<Library> libraries;
 
     public InputParser() {
-        libraries = new ArrayList<Library>();
+        books = new HashMap<>();
+        libraries = new ArrayList<>();
     }
 
+    // sanity check
+//    public static void main(String[] args) throws IOException {
+//        InputParser parser = new InputParser();
+//        String filename = "/home/connor/Downloads/a_example.txt";
+//        parser.parseInput(filename);
+//        System.out.println(parser.numBooks);
+//        System.out.println(parser.numLibraries);
+//        System.out.println(parser.numDays);
+//    }
+
+
+    // no safety checks here sorry team
+    public void parseInput(String filename) throws IOException {
+        Scanner scanner = new Scanner(new File(filename));
+        // First Line of Input
+        numBooks = scanner.nextInt();
+        numLibraries = scanner.nextInt();
+        numDays = scanner.nextInt();
+
+        // books
+        for (int i=0; i<numBooks; i++) {
+            Book currentBook = new Book(i, scanner.nextInt());
+            books.put(i, currentBook);
+        }
+
+        // library sections
+        for (int l=0; l<numLibraries; l++) {
+            int numBooksInLibrary = scanner.nextInt();
+            int daysToSignup = scanner.nextInt();
+            int numBooksPerDay = scanner.nextInt();
+            Library currentLibrary = new Library(l, numBooksPerDay, daysToSignup);
+            for (int b=0; b<numBooksInLibrary; b++) {
+                int bookId = scanner.nextInt();
+                currentLibrary.addBook(bookId);
+            }
+            libraries.add(currentLibrary);
+        }
+    }
 }
--- TheRealDeal/src/Library.java
+++ TheRealDeal/src/Library.java
@@ -1,23 +1,24 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Library {
     public int id;
-    public ArrayList<Book> books;
+    public ArrayList<Integer> books;
     public int booksPerDay;
     public int signupTime;
     public boolean signedUp;
 
     public Library(int id, int booksPerDay, int signupTime) {
-        this.books = new ArrayList<Book>();
+        this.books = new ArrayList<>();
         this.id = id;
         this.booksPerDay = booksPerDay;
         this.signupTime = signupTime;
         this.signedUp = false;
     }
 
-    public void addBook(Book b) {
-        books.add(b);
+    public void addBook(int bookId) {
+        books.add(bookId);
     }
 
 }
