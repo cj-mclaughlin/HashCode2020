a0fab7211b4f031fa81028f210693ed42d1000d2
Nour Elmaliki <nourelmaliki@gmail.com>
2020-02-20 14:25:05 -0500

    Update Library.java

--- TheRealDeal/src/Library.java
+++ TheRealDeal/src/Library.java
@@ -1,17 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Library {
     public int id;
     public List<Integer> books;
+    public List<Integer> finalOrder;
     public int booksPerDay;
     public int signupTime;
     public boolean signedUp;
 
     public Library(int id, int booksPerDay, int signupTime) {
-        this.books = new ArrayList<>();
+        this.books = new ArrayList<>(); //have this be sorted when instantiated
         this.id = id;
         this.booksPerDay = booksPerDay;
         this.signupTime = signupTime;
@@ -20,17 +18,26 @@ public class Library {
 
     public void addBook(int bookId) {
         books.add(bookId);
+    }
 
+    public void sortBooks(Map<Integer,Integer> globalBooks){
+        Collections.sort(books,new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return globalBooks.get(o1).compareTo(globalBooks.get(o2));
+            }
+        });
     }
 
     public int extractValue(Set<Integer> toBeScanned,Map<Integer,Integer> globalBooks, int days){
         int daysRemaining = days - signupTime;
         int maxValue = 0;
         int currentBook = 0;
-        //implement sorting rn
+        finalOrder = new ArrayList<>();
         while(daysRemaining!=0 && currentBook<books.size()){
             if(!toBeScanned.contains(books.get(currentBook))){
                 maxValue += globalBooks.get(books.get(currentBook));
+                finalOrder.add(currentBook);
                 daysRemaining--;
                 currentBook++;
             }
